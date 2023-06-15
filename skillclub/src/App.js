a869da7abe625aa900373c47c7a6633a41949931
abii225 <abhayv225@gmail.com>
2023-06-15 05:40:18 +0530

    homepage and signup

--- skillclub/src/App.js
+++ skillclub/src/App.js
@@ -7,8 +7,18 @@ import Allroutes from './AllRoutes/Allroutes';
 function App() {
   return (
    <>
-   <Navbar/>
+   
+   <div style={{display:"flex",justifyContent:"space-between",boxShadow:" rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px"}}>
+    <div className='logo'>
+    <img style={{width:"80px",height:"100px",display:"block"}} src="https://i.postimg.cc/4N6GDq7C/Colorful-Infinite-Logo-Design-1.png"   alt="logo" />
+    <h1>Skill Club</h1>
+    </div>
+    <Navbar/></div>
+   <div style={{marginTop:"100px"}}>
    <Allroutes/>
+   </div>
+
+ 
    </>
   );
 }
--- skillclub/src/Pages/Signup.jsx
+++ skillclub/src/Pages/Signup.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import "../Styles/Signup.css"
+import { Link } from "react-router-dom"
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
+  <>
+  <div id="signupMain">
+    <div>
+      <form class="form">
+    <p class="title">Register </p>
+    <p class="message">Signup now and get full access to our app. </p>
+        <div class="flex">
+        <label>
+            <input required="" placeholder="" type="text" class="input"/>
+            <span>Firstname</span>
+        </label>
+
+        <label>
+            <input required="" placeholder="" type="text" class="input"/>
+            <span>Lastname</span>
+        </label>
+    </div>  
+            
+    <label>
+        <input required="" placeholder="" type="email" class="input"/>
+        <span>Email</span>
+    </label> 
+        
+    <label>
+        <input required="" placeholder="" type="password" class="input"/>
+        <span>Password</span>
+    </label>
+    <label>
+        <input required="" placeholder="" type="password" class="input"/>
+        <span>Confirm password</span>
+    </label>
+    <button class="submit">Submit</button>
+    <p class="signin">Already have an acount ? <Link to="/login">login</Link></p>
+</form>
+    </div>
+   <div>
+    <img src="https://i.pinimg.com/564x/d8/5f/2e/d85f2e49bf6d1bce9f81fd7a0ee3fcb5.jpg" alt="signupImage" />
+   </div>
+    </div>
+    </>)
 }
 
 export default Signup
